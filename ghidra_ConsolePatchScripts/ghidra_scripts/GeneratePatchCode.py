a60b991217e5f686ac6a34c49ace74b1535c7eb4
illusion0001 <37698908+illusion0001@users.noreply.github.com>
2022-03-24 15:03:38 -0500

    Autospacing for x86 patch
    + compare typo fixes

--- ghidra_ConsolePatchScripts/ghidra_scripts/GeneratePatchCode.py
+++ ghidra_ConsolePatchScripts/ghidra_scripts/GeneratePatchCode.py
@@ -15,10 +15,14 @@
 from binascii import hexlify
 
 def getdata(codeUnit):
-    address        = str(codeUnit.getAddress())
-    value          = str(hexlify(codeUnit.getBytes()))
-    oprand_comment = str(codeUnit.toString())
+    address        = (codeUnit.getAddress())
+    value          = (hexlify(codeUnit.getBytes()))
+    oprand_comment = (codeUnit.toString())
     return address, value, oprand_comment
+
+# https://blog.finxter.com/how-to-find-the-longest-string-in-a-python-list
+def get_max_str(lst):
+    return max(lst, key=len)
 
 def gen_patch():
         # https://github.com/lwerdna/ghidra/blob/master/XorMemoryScript.py
@@ -33,31 +37,38 @@ def gen_patch():
         minAddr   = str(currentProgram.getMinAddress())
         codeUnits = listing.getCodeUnits(addrSet, True)
 
-        if processor.startswith('PowerPC:BE:64:64-32addr' or 'PowerPC:BE:64:A2-32addr'): # PS3
-            print('Platform is PS3.')
+        if processor == 'PowerPC:BE:64:64-32addr' or processor == 'PowerPC:BE:64:A2-32addr': # PS3
+            print('Platform is PS3 ({0}).'.format(processor))
             for codeUnit in codeUnits:
                 getdata(codeUnit)
                 addr, val, oprand = getdata(codeUnit)
                 print('- [ be32, 0x{0}, 0x{1} ] # {2}'.format(addr, val, oprand))
-        elif processor.startswith('PowerPC:BE:64:VLE-32addr'): # X360
-            print('Platform is Xbox 360.')
+        elif processor == 'PowerPC:BE:64:VLE-32addr': # X360
+            print('Platform is Xbox 360 ({0}).'.format(processor))
             for codeUnit in codeUnits:
                 getdata(codeUnit)
                 addr, val, oprand = getdata(codeUnit)
                 print('    [[patch.be32]]\n'
                       '        address = 0x{0}\n'
                       '        value = 0x{1} # {2}'.format(addr, val, oprand))
-        elif processor.startswith('x86:LE:64:default'):
-            if minAddr.startswith('00400000'):
+        elif processor == 'x86:LE:64:default':
+            if minAddr == '00400000':
                 # use 0x400000 (disabled aslr) addr for now
                 # who knows what the correct one will be
                 print('Image Base {} is correct.'.format(minAddr))
-                print('Platform is PS4.')
+                print('Platform is PS4 ({0}).'.format(processor))
+                patch_list  = []
+                oprand_list = []
                 for codeUnit in codeUnits:
                     getdata(codeUnit)
                     addr, val, oprand = getdata(codeUnit)
-                    print('- [ bytes, 0x{0}, \"{1:<20}\" ] # {2}'.format(addr, val, oprand))
-                    # align to left with spaces if less than 20 chars
+                    patch = '- [ bytes, 0x{0}, \"{1}\" ]'.format(addr, val) # thanks aero+kiwi
+                    patch_list.append(patch)
+                    oprand_list.append(oprand)
+                length = (get_max_str(patch_list))
+                real_length = (len(length))
+                for patch, oprand in zip(patch_list, oprand_list):
+                    print('{0:<{2}} # {1}'.format(patch, oprand, real_length))
             else:
                 print('Image Base {} is not correct, patch address will be wrong! Make sure it is set to 0x400000.\nExiting script.'.format(minAddr))
                 return
